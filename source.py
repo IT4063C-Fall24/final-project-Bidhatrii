23783921db67c4e6069a976f9d7557342e36cf53
Bidhatri Amatya <abidhatri@gmail.com>
2024-10-24 14:36:02 -0400

    Added datasets relation and analysis approach

--- source.py
+++ source.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # {Project Title}📝
+# # Exploring the Gender Pay Gap Across Industries
 # 
 # ![Banner](./assets/banner.jpeg)
 
 # ## Topic
 # *What problem are you (or your stakeholder) trying to address?*
 # 📝 <!-- Answer Below -->
+# 
+# Despite progress towards gender equality in the workplace, significant disparities in pay between men and women persist across various industries. Understanding the factors contributing to these disparities is essential for addressing the gender pay gap effectively.
+# 
+# The gender pay gap impacts economic stability and personal well-being for many individuals. Addressing it can lead to a more equitable workforce and improved societal outcomes.
+# 
+# 
 
 # ## Project Question
 # *What specific question are you seeking to answer with this project?*
 # *This is not the same as the questions you ask to limit the scope of the project.*
 # 📝 <!-- Answer Below -->
+# 
+# -What is the average salary difference between men and women in various industries?
+# -How do education levels and years of experience correlate with the gender pay gap?
+# -Are there specific industries where the gender pay gap is more pronounced?
+# -What role do company diversity and inclusion initiatives play in mitigating the pay gap?
 
 # ## What would an answer look like?
 # *What is your hypothesized answer to your question?*
 # 📝 <!-- Answer Below -->
+# 
+# -Visual representations such as bar charts comparing average salaries by gender across industries.
+# -Scatter plots correlating years of experience and education level with salary differences.
+# -A summary of findings highlighting industries with significant disparities and potential factors influencing these gaps.
 
 # ## Data Sources
 # *What 3 data sources have you identified for this project?*
@@ -38,9 +53,9 @@
 # *What resources and references have you used for this project?*
 # 📝 <!-- Answer Below -->
 
-# In[ ]:
+# In[2]:
 
 
 # ⚠️ Make sure you run this cell at the end of your notebook before every submission!
-get_ipython().system('jupyter nbconvert --to python python-exercises.ipynb')
+get_ipython().system('jupyter nbconvert --to python source.ipynb')
 
